5693e5102a97a9cf3614aa48befc961bb48277ef
vishalbanderwale <vishalsonu945@gmail.com>
2023-05-26 00:20:03 +0530

    ecommerce work in progress

---
+++ src/pages/CartListingpage/CartListingpage.jsx
@@ -0,0 +1,14 @@
+import CartCard from "../../components/CartCard/CartCard"
+
+
+
+function CartListingPage(){
+    return(
+        <div>
+<CartCard/>
+s
+
+        </div>
+    )
+}
+export default CartListingPage
